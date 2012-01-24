ee18cbbd040efe1353337fd5d42cb61a1d8c821c
Timothé Fillion Brunet <timothefilb@gmail.com>
2012-01-23 19:50:49 -0500

    update

---
+++ Labo1/Labo1Tests/Labo1Tests.h
@@ -0,0 +1,13 @@
+//
+//  Labo1Tests.h
+//  Labo1Tests
+//
+//  Created by Timothé Fillion Brunet on 12-01-23.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Labo1Tests : SenTestCase
+
+@end
