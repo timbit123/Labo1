391359c4aa4326d3a7a9c7f3bcf25ab1fe6f86a1
Timothé Fillion Brunet <timothefilb@gmail.com>
2012-01-30 19:22:13 -0500

    init

---
+++ Labo1/Labo1/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Labo1
+//
+//  Created by Timothé Fillion Brunet on 12-01-30.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
