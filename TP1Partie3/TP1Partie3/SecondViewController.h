80a5b35d57cf207cdadf5080028021280fd7a0f8
Alexandre Henry-Lebel <henry.alex3@gmail.com>
2012-02-05 18:37:52 -0500

    asdfasdf

--- TP1Partie3/TP1Partie3/SecondViewController.h
+++ TP1Partie3/TP1Partie3/SecondViewController.h
@@ -10,10 +10,26 @@
 #import "Competition.h"
 
 @interface SecondViewController : UIViewController<UITableViewDelegate,UITableViewDataSource>
+{
+    bool isDescente;
+}
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *btnDemarrer;
 @property (weak, nonatomic) IBOutlet UIBarButtonItem *btnDNF;
 - (IBAction)demarrerClick:(id)sender;
 - (IBAction)dnfClick:(id)sender;
 @property (weak, nonatomic) IBOutlet UILabel *lblCourant;
+@property (weak, nonatomic) IBOutlet UITableView *lstAttente;
+@property (weak, nonatomic) IBOutlet UIButton *btnConfirmer;
+@property (weak, nonatomic) IBOutlet UITextField *txtJuge1;
+@property (weak, nonatomic) IBOutlet UITextField *txtJuge2;
+@property (weak, nonatomic) IBOutlet UITextField *txtJuge3;
+@property (weak, nonatomic) IBOutlet UITextField *txtJuge4;
+@property (weak, nonatomic) IBOutlet UITextField *txtJuge5;
+@property (weak, nonatomic) IBOutlet UITextField *txtJuge6;
+@property (weak, nonatomic) IBOutlet UITextField *txtJuge7;
+
+- (IBAction)continuerClick:(id)sender;
+
+
 
 @end
--- TP1Partie3/TP1Partie3/SecondViewController.m
+++ TP1Partie3/TP1Partie3/SecondViewController.m
@@ -10,6 +10,15 @@
 
 @implementation SecondViewController
 @synthesize lblCourant;
+@synthesize lstAttente;
+@synthesize btnConfirmer;
+@synthesize txtJuge1;
+@synthesize txtJuge2;
+@synthesize txtJuge3;
+@synthesize txtJuge4;
+@synthesize txtJuge5;
+@synthesize txtJuge6;
+@synthesize txtJuge7;
 @synthesize btnDemarrer;
 @synthesize btnDNF;
 
@@ -25,6 +34,7 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    isDescente = NO;
 }
 
 - (void)viewDidUnload
@@ -32,6 +42,15 @@
     [self setBtnDemarrer:nil];
     [self setBtnDNF:nil];
     [self setLblCourant:nil];
+    [self setLstAttente:nil];
+    [self setBtnConfirmer:nil];
+    [self setTxtJuge1:nil];
+    [self setTxtJuge2:nil];
+    [self setTxtJuge3:nil];
+    [self setTxtJuge4:nil];
+    [self setTxtJuge5:nil];
+    [self setTxtJuge6:nil];
+    [self setTxtJuge7:nil];
     [super viewDidUnload];
     // Release any retained subviews of the main view.
     // e.g. self.myOutlet = nil;
@@ -71,7 +90,7 @@
 
 // Customize the number of rows in the table view.
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-	return [[[Competition laCompetition] getLstAthlete] count]+1;
+	return [[[Competition laCompetition] getLstAthleteAttente] count]+1;
 }
 
 
@@ -120,11 +139,140 @@
 }
 
 - (IBAction)demarrerClick:(id)sender {
-    Athlete *courant = [[Competition laCompetition] getAthleteCourant];
-    [lblCourant setText:[NSString stringWithFormat:@"#%d - %@ %@ du pays: %@", [courant numero], [courant prenom], [courant nom], [courant pays]]];
-    [btnDemarrer setTitle:@"Fin de la descente"];
+    if(isDescente){
+        [btnDemarrer setTitle:@"Démarrer descente"];
+        [btnDemarrer setEnabled:NO];
+        [btnDNF setEnabled:NO];
+        [btnConfirmer setEnabled:YES];
+        isDescente = NO;
+        
+        [txtJuge1 setEnabled:YES];
+        [txtJuge2 setEnabled:YES];
+        [txtJuge3 setEnabled:YES];
+        [txtJuge4 setEnabled:YES];
+        [txtJuge5 setEnabled:YES];
+        [txtJuge6 setEnabled:YES];
+        [txtJuge7 setEnabled:YES];
+        
+        UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle:@"La descente est terminée. Veuillez entrer le résultat des juges."
+                              message:nil
+                              delegate:nil
+                              cancelButtonTitle:nil
+                              otherButtonTitles:@"OK", nil];
+        [alert show];
+    } else {
+        Athlete *courant = [[Competition laCompetition] getAthleteCourant];
+        [lblCourant setText:[NSString stringWithFormat:@"#%d - %@ %@ du pays: %@", [courant numero], [courant prenom], [courant nom], [courant pays]]];
+        [btnDemarrer setTitle:@"Fin de la descente"];
+        [btnDNF setEnabled:YES];
+        [lstAttente reloadData];
+        [btnConfirmer setEnabled:NO];
+        isDescente = YES;
+        
+        [txtJuge1 setEnabled:NO];
+        [txtJuge2 setEnabled:NO];
+        [txtJuge3 setEnabled:NO];
+        [txtJuge4 setEnabled:NO];
+        [txtJuge5 setEnabled:NO];
+        [txtJuge6 setEnabled:NO];
+        [txtJuge7 setEnabled:NO];
+    }
+    
 }
 
 - (IBAction)dnfClick:(id)sender {
+    [[Competition laCompetition] setPointageAthleteCourant:-1 :1.0f];
+    [btnDemarrer setTitle:@"Démarrer descente"];
+    [btnDemarrer setEnabled:YES];
+    [btnDNF setEnabled:NO];
+    [btnConfirmer setEnabled:NO];
+    isDescente = NO;
+    
+    [lblCourant setText:@"Aucun"];
+    
+    [txtJuge1 setEnabled:NO];
+    [txtJuge2 setEnabled:NO];
+    [txtJuge3 setEnabled:NO];
+    [txtJuge4 setEnabled:NO];
+    [txtJuge5 setEnabled:NO];
+    [txtJuge6 setEnabled:NO];
+    [txtJuge7 setEnabled:NO];
+}
+- (IBAction)continuerClick:(id)sender {
+    
+    if(txtJuge1.text.length > 0 && txtJuge2.text.length > 0 && txtJuge3.text.length > 0 && txtJuge4.text.length > 0 && txtJuge5.text.length > 0 && txtJuge6.text.length > 0 && txtJuge7.text.length > 0){
+        
+        if([[NSScanner scannerWithString:txtJuge1.text] scanInt:NULL] && [[NSScanner scannerWithString:txtJuge2.text] scanInt:NULL] && [[NSScanner scannerWithString:txtJuge3.text] scanInt:NULL] && [[NSScanner scannerWithString:txtJuge1.text] scanInt:NULL] && [[NSScanner scannerWithString:txtJuge4.text] scanInt:NULL] && [[NSScanner scannerWithString:txtJuge5.text] scanInt:NULL] && [[NSScanner scannerWithString:txtJuge6.text] scanInt:NULL] && [[NSScanner scannerWithString:txtJuge7.text] scanInt:NULL]){
+            int p1 = [txtJuge1.text intValue];
+            int p2 = [txtJuge2.text intValue];
+            int p3 = [txtJuge3.text intValue];
+            int p4 = [txtJuge4.text intValue];
+            int p5 = [txtJuge5.text intValue];
+            int p6 = [txtJuge6.text intValue];
+            int p7 = [txtJuge7.text intValue];
+            
+            if((p1>=0&&p1<=5)&&(p2>=0&&p2<=5)&&(p3>=0&&p3<=5)&&(p4>=0&&p4<=5)&&(p5>=0&&p5<=5)&&(p6>=0&&p6<=5)&&(p7>=0&&p7<=5)){
+                //array, sort, enleve extremités
+                NSArray *arr, *sorted;
+                arr = [NSArray arrayWithObjects:[NSNumber numberWithInt:p1], [NSNumber numberWithInt:p2], [NSNumber numberWithInt:p3], [NSNumber numberWithInt:p4], [NSNumber numberWithInt:p5], [NSNumber numberWithInt:p6], [NSNumber numberWithInt:p7], nil];
+                sorted = [arr sortedArrayUsingSelector:@selector(compare:)];
+                
+                int total = 0;
+                for(int i = 1; i<=sorted.count-1; i++){
+                    total += (int)[sorted objectAtIndex:i];
+                }
+                
+                [[Competition laCompetition] setPointageAthleteCourant:total :5.0f];
+                //SI DERNIER -> MÉDAILLE
+                [btnDemarrer setEnabled:YES];
+                [btnConfirmer setEnabled:NO];
+                [txtJuge1 setEnabled:NO];
+                [txtJuge2 setEnabled:NO];
+                [txtJuge3 setEnabled:NO];
+                [txtJuge4 setEnabled:NO];
+                [txtJuge5 setEnabled:NO];
+                [txtJuge6 setEnabled:NO];
+                [txtJuge7 setEnabled:NO];
+                
+                
+                [txtJuge1 setText:@""];
+                [txtJuge2 setText:@""];
+                [txtJuge3 setText:@""];
+                [txtJuge4 setText:@""];
+                [txtJuge5 setText:@""];
+                [txtJuge6 setText:@""];
+                [txtJuge7 setText:@""];
+                
+                [lblCourant setText:@"Aucun"];
+            } else {
+                UIAlertView *alert = [[UIAlertView alloc]
+                                      initWithTitle:@"Le résultat doit être un entier entre 0 et 5."
+                                      message:nil
+                                      delegate:nil
+                                      cancelButtonTitle:nil
+                                      otherButtonTitles:@"OK", nil];
+                [alert show];
+            }
+        } else {
+            UIAlertView *alert = [[UIAlertView alloc]
+                                  initWithTitle:@"Le résultat doit être un entier entre 0 et 5."
+                                  message:nil
+                                  delegate:nil
+                                  cancelButtonTitle:nil
+                                  otherButtonTitles:@"OK", nil];
+            [alert show];
+        
+        }
+    } else {
+        UIAlertView *alert = [[UIAlertView alloc]
+                              initWithTitle:@"Veuillez remplir tous les champs pour confirmer le pointage."
+                              message:nil
+                              delegate:nil
+                              cancelButtonTitle:nil
+                              otherButtonTitles:@"OK", nil];
+        [alert show];
+    }
+    
 }
 @end
